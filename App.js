0be62e252beb89711f83fee2506818db98e12c46
adyprajwal <prajwal.ady@gmail.com>
2020-03-01 13:19:26 +0545

    configure the layout

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import { StyleSheet, Text, View } from "react-native";
+import { AppLoading } from "expo";
+import HomeStack from "./routes/homeStack";
+
+const getFonts = () => {
+	return Fonts.loadAsync({
+		"exo2-regular": require("./assets/fonts/Exo2-Regular.ttf"),
+		"exo2-bold": require("./assets/fonts/Exo2-Bold.ttf")
+	});
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	const [fontsLoaded, setFontsLoaded] = useState(false);
+	if (fontsLoaded) {
+		return <HomeStack />;
+	} else {
+		return (
+			<AppLoading startAsync={getFonts} onFinish={setFontsLoaded(true)} />
+		);
+	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/home.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import {
+	StyleSheet,
+	View,
+	Text,
+	FlatList,
+	TouchableOpacity
+} from "react-native";
+
+export default function Home() {
+	return (
+		<View>
+			<Text>Home Screen</Text>
+		</View>
+	);
+}
