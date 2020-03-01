80c45b98fdcce955af877fdaf28e5339803953dc
adyprajwal <prajwal.ady@gmail.com>
2020-03-01 13:19:26 +0545

    configure the layout

---
+++ routes/homeStack.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "../screens/home";
+
+const Stack = createStackNavigator();
+
+export default function HomeStack() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Home">
+				<Stack.Screen
+					name="Home"
+					component={Home}
+					options={{ title: "Home" }}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
