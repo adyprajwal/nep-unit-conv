c7854c7fa671e94e2eba176026b07384412857d3
adyprajwal <prajwal.ady@gmail.com>
2020-03-02 14:35:36 +0545

    add custom header component for about screen

---
+++ components/aboutHeader.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+export default function AboutHeader({ navigation, title }) {
+	const goToPreviousScreen = () => {
+		navigation.goBack();
+	};
+	return (
+		<View style={styles.header}>
+			<MaterialIcons
+				name="arrow-back"
+				size={28}
+				style={styles.icon}
+				onPress={goToPreviousScreen}
+			/>
+			<View style={styles.headerTitle}>
+				<Text style={styles.headerText}>{title}</Text>
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	header: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	headerTitle: {
+		flexDirection: "row",
+		alignItems: "center",
+		marginLeft: 15
+	},
+	headerText: {
+		fontSize: 20,
+		color: "#000"
+	},
+	icon: {
+		color: "#000"
+	}
+});
---
+++ screens/about.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+export default function About() {
+	return (
+		<View>
+			<Text>Nepali Unit Converter App</Text>
+			<Text> Version: 0.0.1</Text>
+		</View>
+	);
+}
