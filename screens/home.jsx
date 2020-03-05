7d431b6b51f5d0bda46f0bf66f1a0d30551405be
adyprajwal <prajwal.ady@gmail.com>
2020-03-05 16:58:15 +0545

    convert to all other units -> aana, paisa, daam, bigha, kattha, dhur, sqmtr, sqft

--- screens/home.jsx
+++ screens/home.jsx
@@ -43,15 +43,95 @@ export default function Home() {
 
 	const convert = (val, { unit }) => {
 		if (unit == "ropani") {
-			setRopani(val);
-			setAana((val * 16).toFixed(3));
-			setPaisa((val * 64).toFixed(3));
-			setDaam((val * 256).toFixed(3));
-			setBigha((val / 13.31).toFixed(3));
-			setKattha((val / 1.5).toFixed(3));
-			setDhur((val / 30.05).toFixed(3));
-			setSqmtr((val * 508.72).toFixed(3));
-			setSqft((val * 5476).toFixed(3));
+			setRopani(+(val * 1).toFixed(5));
+			setAana(+(val * 16).toFixed(5));
+			setPaisa(+(val * 64).toFixed(5));
+			setDaam(+(val * 256).toFixed(5));
+			setBigha(+(val / 13.31264).toFixed(5));
+			setKattha(+(val * 1.50233).toFixed(5));
+			setDhur(+(val * 30.04664).toFixed(5));
+			setSqmtr(+(val * 508.72).toFixed(5));
+			setSqft(+(val * 5476).toFixed(5));
+		} else if (unit == "aana") {
+			setRopani(+(val / 16).toFixed(5));
+			setAana(+(val * 1).toFixed(5));
+			setPaisa(+(val * 4).toFixed(5));
+			setDaam(+(val * 16).toFixed(5));
+			setBigha(+(val / 213.0022).toFixed(5));
+			setKattha(+(val / 10.6501).toFixed(5));
+			setDhur(+(val * 1.87791).toFixed(5));
+			setSqmtr(+(val * 31.8).toFixed(5));
+			setSqft(+(val * 342.25).toFixed(5));
+		} else if (unit == "paisa") {
+			setRopani(+(val / 64).toFixed(5));
+			setAana(+(val / 4).toFixed(5));
+			setPaisa(+(val * 1).toFixed(5));
+			setDaam(+(val * 4).toFixed(5));
+			setBigha(+(val / 852.03366).toFixed(5));
+			setKattha(+(val / 42.60168).toFixed(5));
+			setDhur(+(val / 2.13008).toFixed(5));
+			setSqmtr(+(val * 85.56).toFixed(5));
+			setSqft(+(val * 7.95).toFixed(5));
+		} else if (unit == "daam") {
+			setRopani(+(val / 256).toFixed(5));
+			setAana(+(val / 16).toFixed(5));
+			setPaisa(+(val / 4).toFixed(5));
+			setDaam(+(val * 1).toFixed(5));
+			setBigha(+(val / 3408.13464).toFixed(5));
+			setKattha(+(val / 170.40673).toFixed(5));
+			setDhur(+(val / 8.52034).toFixed(5));
+			setSqmtr(+(val * 1.99).toFixed(5));
+			setSqft(+(val * 21.39).toFixed(5));
+		} else if (unit == "bigha") {
+			setRopani(+(val * 13.31264).toFixed(5));
+			setAana(+(val * 213.022).toFixed(5));
+			setPaisa(+(val * 852.03366).toFixed(5));
+			setDaam(+(val * 3408.13464).toFixed(5));
+			setBigha(+(val * 1).toFixed(5));
+			setKattha(+(val * 20).toFixed(5));
+			setDhur(+(val * 400).toFixed(5));
+			setSqmtr(+(val * 6772.63).toFixed(5));
+			setSqft(+(val * 72900).toFixed(5));
+		} else if (unit == "kattha") {
+			setRopani(+(val / 1.50233).toFixed(5));
+			setAana(+(val * 10.6501).toFixed(5));
+			setPaisa(+(val * 42.60168).toFixed(5));
+			setDaam(+(val * 170.40673).toFixed(5));
+			setBigha(+(val / 20).toFixed(5));
+			setKattha(+(val * 1).toFixed(5));
+			setDhur(+(val * 20).toFixed(5));
+			setSqmtr(+(val * 338.63).toFixed(5));
+			setSqft(+(val * 3645).toFixed(5));
+		} else if (unit == "dhur") {
+			setRopani(+(val / 30.04664).toFixed(5));
+			setAana(+(val / 1.87791).toFixed(5));
+			setPaisa(+(val * 2.13008).toFixed(5));
+			setDaam(+(val * 8.52034).toFixed(5));
+			setBigha(+(val / 400).toFixed(5));
+			setKattha(+(val / 20).toFixed(5));
+			setDhur(+(val * 1).toFixed(5));
+			setSqmtr(+(val * 16.93).toFixed(5));
+			setSqft(+(val * 182.25).toFixed(5));
+		} else if (unit == "sqmtr") {
+			setRopani(+(val / 508.72).toFixed(5));
+			setAana(+(val / 31.8).toFixed(5));
+			setPaisa(+(val / 7.95).toFixed(5));
+			setDaam(+(val / 1.99).toFixed(5));
+			setBigha(+(val / 6772.63).toFixed(5));
+			setKattha(+(val / 338.63).toFixed(5));
+			setDhur(+(val / 16.93).toFixed(5));
+			setSqmtr(+(val * 1).toFixed(5));
+			setSqft(+(val * 10.7639).toFixed(5));
+		} else if (unit == "sqft") {
+			setRopani(+(val / 5476).toFixed(5));
+			setAana(+(val / 342.25).toFixed(5));
+			setPaisa(+(val / 85.56).toFixed(5));
+			setDaam(+(val / 21.39).toFixed(5));
+			setBigha(+(val / 72900).toFixed(5));
+			setKattha(+(val / 3645).toFixed(5));
+			setDhur(+(val / 182.25).toFixed(5));
+			setSqmtr(+(val / 10.7639).toFixed(5));
+			setSqft(+(val * 1).toFixed(5));
 		}
 	};
 
