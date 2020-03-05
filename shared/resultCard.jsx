15abafa7b2a4f1874d4e83194a853820b688ec0c
adyprajwal <prajwal.ady@gmail.com>
2020-03-05 13:23:40 +0545

    convert to all units when input is ropani

---
+++ shared/resultCard.jsx
@@ -0,0 +1,7 @@
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, FlatList, TouchableOpacity} from 'react-native';
+
+export default function ResultCard() {
+    return();
+}
+const styles = StyleSheet.create({});
