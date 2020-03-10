fd715e7c55f562841bce70dce971ed4b81e96e8f
adyprajwal <prajwal.ady@gmail.com>
2020-03-10 13:50:29 +0545

    create separate results component

--- components/shared/resultCard.jsx
+++ components/shared/resultCard.jsx
@@ -1,7 +1,35 @@
-import React, {useState} from 'react';
-import {StyleSheet, View, Text, FlatList, TouchableOpacity} from 'react-native';
+import React, { useState } from "react";
+import {
+	StyleSheet,
+	View,
+	Text,
+	FlatList,
+	TouchableOpacity
+} from "react-native";
 
-export default function ResultCard() {
-    return();
+export default function ResultCard({
+	ropani,
+	aana,
+	paisa,
+	daam,
+	bigha,
+	kattha,
+	dhur,
+	sqmtr,
+	sqft
+}) {
+	return (
+		<View>
+			<Text>Ropani: {ropani}</Text>
+			<Text>Aana: {aana}</Text>
+			<Text>Paisa: {paisa}</Text>
+			<Text>Daam: {daam}</Text>
+			<Text>Bigha: {bigha}</Text>
+			<Text>Kattha: {kattha}</Text>
+			<Text>Dhur: {dhur}</Text>
+			<Text>Square Meter: {sqmtr}</Text>
+			<Text>Square Feet: {sqft}</Text>
+		</View>
+	);
 }
 const styles = StyleSheet.create({});
