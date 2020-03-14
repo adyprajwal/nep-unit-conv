6d535a18e14d425d5b1a66e266c8457fdb5ad9b6
adyprajwal <prajwal.ady@gmail.com>
2020-03-14 17:21:40 +0545

    add settings screen and rename secondary header component

---
+++ screens/settings.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { globalStyles } from "../styles/globalStyles";
+
+export default function Settings() {
+	return (
+		<View style={globalStyles.container}>
+			<Text>This is Settings Screen</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({});
