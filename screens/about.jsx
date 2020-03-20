1a79b09d3bfbd2bec1cae60309fa08dc68c4be97
adyprajwal <prajwal.ady@gmail.com>
2020-03-20 12:14:40 +0545

    change description and version number in about screen

--- screens/about.jsx
+++ screens/about.jsx
@@ -5,8 +5,22 @@ import { globalStyles } from "../styles/globalStyles";
 export default function About() {
 	return (
 		<View style={globalStyles.container}>
-			<Text>Nepali Unit Converter App</Text>
-			<Text> Version: 0.7</Text>
+			<Text style={styles.titleText}>Nepali Unit Converter</Text>
+			<Text style={styles.versionText}> Version: 1.0</Text>
+			<Text>
+				This app intends to simplify the conversion of traditional
+				Nepali Measurement Units into well known metric as well as
+				imperial system.
+			</Text>
 		</View>
 	);
 }
+const styles = StyleSheet.create({
+	titleText: {
+		fontWeight: "bold",
+		fontSize: 18
+	},
+	versionText: {
+		fontSize: 18
+	}
+});
