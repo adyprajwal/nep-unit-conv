bb8a89b0ccada19db9568429618aa9e2704060da
adyprajwal <prajwal.ady@gmail.com>
2020-03-21 19:48:53 +0545

    add custom drawer header

--- components/customDrawerContent.jsx
+++ components/customDrawerContent.jsx
@@ -4,12 +4,42 @@ import {
 	DrawerItemList,
 	DrawerItem
 } from "@react-navigation/drawer";
+import { StyleSheet, Image, View, Text } from "react-native";
 
 export default function CustomDrawerContent(props) {
 	return (
 		<DrawerContentScrollView {...props}>
+			<View style={styles.drawerHeader}>
+				<Image
+					style={styles.drawerImage}
+					source={require("../assets/icon.jpg")}
+				/>
+				<View>
+					<Text style={styles.drawerHeaderTitle}>
+						Nepali Unit Converter{" "}
+					</Text>
+				</View>
+			</View>
+
 			{/* <DrawerItem label="Help" /> */}
 			<DrawerItemList {...props} />
 		</DrawerContentScrollView>
 	);
 }
+const styles = StyleSheet.create({
+	drawerHeader: {
+		height: 180,
+		backgroundColor: "#e84f54"
+	},
+	drawerHeaderTitle: {
+		height: 30,
+		color: "#ddd",
+		fontFamily: "exo2-regular",
+		fontSize: 20
+	},
+	drawerImage: {
+		height: 80,
+		width: 80,
+		borderRadius: 10
+	}
+});
