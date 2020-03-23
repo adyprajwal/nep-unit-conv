9392ee199b5ae128945e7e73d8393adfcda26fdf
adyprajwal <prajwal.ady@gmail.com>
2020-03-23 10:28:57 +0545

    design fixes for custom drawerHeader

--- components/customDrawerContent.jsx
+++ components/customDrawerContent.jsx
@@ -8,38 +8,48 @@ import { StyleSheet, Image, View, Text } from "react-native";
 
 export default function CustomDrawerContent(props) {
 	return (
-		<DrawerContentScrollView {...props}>
+		<DrawerContentScrollView {...props} contentContainerStyle={{ flex: 1 }}>
 			<View style={styles.drawerHeader}>
 				<Image
 					style={styles.drawerImage}
 					source={require("../assets/icon.jpg")}
 				/>
-				<View>
-					<Text style={styles.drawerHeaderTitle}>
-						Nepali Unit Converter{" "}
-					</Text>
-				</View>
+				<Text style={styles.drawerHeaderTitle}>
+					Nepali Unit Converter
+				</Text>
 			</View>
 
-			{/* <DrawerItem label="Help" /> */}
+			{/* <DrawerItem
+				label="Help"
+				onPress={() => props.navigation.navigate("Length")}
+			/> */}
 			<DrawerItemList {...props} />
 		</DrawerContentScrollView>
 	);
 }
 const styles = StyleSheet.create({
+	drawerContainer: {
+		flex: 1
+	},
 	drawerHeader: {
-		height: 180,
-		backgroundColor: "#e84f54"
+		height: 200,
+		// backgroundColor: "#e84f54",
+		backgroundColor: "#e3e1e1",
+		justifyContent: "flex-end",
+		marginTop: -50
 	},
 	drawerHeaderTitle: {
 		height: 30,
-		color: "#ddd",
-		fontFamily: "exo2-regular",
+		color: "#000",
+		marginVertical: 10,
+		marginLeft: 10,
+		fontFamily: "exo2-bold",
 		fontSize: 20
 	},
 	drawerImage: {
 		height: 80,
 		width: 80,
-		borderRadius: 10
+		marginLeft: 10,
+		borderRadius: 50
 	}
 });
