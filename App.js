5f3eabed25b7bc084fe1ee139b0c857c04713337
adyprajwal <prajwal.ady@gmail.com>
2020-03-28 23:42:12 +0545

    change App.js file to conform to react-native dependency

--- App.js
+++ App.js
@@ -1,25 +1,6 @@
-import React, { useState } from "react";
-import * as Font from "expo-font";
-import { AppLoading } from "expo";
+import React from "react";
 import Navigator from "./routes/navigator";
 
-const getFonts = () => {
-	return Font.loadAsync({
-		"exo2-regular": require("./assets/fonts/Exo2-Regular.ttf"),
-		"exo2-bold": require("./assets/fonts/Exo2-Bold.ttf")
-	});
-};
-
 export default function App() {
-	const [fontsLoaded, setFontsLoaded] = useState(false);
-	if (fontsLoaded) {
-		return <Navigator />;
-	} else {
-		return (
-			<AppLoading
-				startAsync={getFonts}
-				onFinish={() => setFontsLoaded(true)}
-			/>
-		);
-	}
+	return <Navigator />;
 }
