2df7ece7c4bef33e30ebbbf704ba4ba2488f74fa
adyprajwal <prajwal.ady@gmail.com>
2020-03-29 21:11:55 +0545

    add splash screen

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
+import SplashScreen from "react-native-splash-screen";
 import Navigator from "./routes/navigator";
 
 export default function App() {
+	useEffect(() => {
+		SplashScreen.hide();
+	});
 	return <Navigator />;
 }
---
+++ android/app/src/main/java/com/nep_unit_conv/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.nep_unit_conv;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
