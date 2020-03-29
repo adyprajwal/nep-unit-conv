733c410c0c3094042f3e9e46b7c66834ab60c193
adyprajwal <prajwal.ady@gmail.com>
2020-03-29 21:11:55 +0545

    add splash screen

--- android/app/src/main/java/com/nep_unit_conv/MainActivity.java
+++ android/app/src/main/java/com/nep_unit_conv/MainActivity.java
@@ -1,8 +1,9 @@
 package com.nep_unit_conv;
-
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import org.devio.rn.splashscreen.SplashScreen;  //changes
+import android.os.Bundle; //changes
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
@@ -11,6 +12,12 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
+    
     @Override
     protected String getMainComponentName() {
         return "nep_unit_conv";
