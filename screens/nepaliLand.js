fb9aa1f40211d4060cb0be2fe7aa80a7cf8d36e9
Prajwal Adhikari <prajwal.ady@gmail.com>
2020-08-24 14:33:35 +0545

    Add input picker

--- screens/nepaliLand.js
+++ screens/nepaliLand.js
@@ -1,14 +1,77 @@
 import React, { useState } from "react";
-import { View, Text, StatusBar } from "react-native";
+import { View, Text, StatusBar, StyleSheet } from "react-native";
 import { globalStyles } from "../styles/globalStyles";
-import InputCard from "../components/shared/inputCard";
-import ResultCard from "../components/shared/resultCard";
+import RNPickerSelect from "react-native-picker-select";
 
 export default function NepaliLand() {
 	return (
 		<View style={globalStyles.container}>
 			<StatusBar backgroundColor="#b05643" />
-			<Text>Nepali Land Converter</Text>
+			<View style={styles.inputView}>
+				<View style={styles.inputUnitView}>
+					<View style={styles.inputUnitLabel}>
+						<Text
+							style={{
+								fontFamily: "Exo2-Bold",
+								fontSize: 17,
+								color: "gray",
+							}}
+						>
+							Input Unit
+						</Text>
+					</View>
+					<View style={styles.inputUnitselectorView}>
+						<RNPickerSelect
+							onValueChange={(value) => console.log(value)}
+							items={[
+								{
+									label: "Ropani | Aana | Paisa | Daam",
+									value: "rapd",
+								},
+								{
+									label: "Bigha | Kattha | Dhur",
+									value: "bkd",
+								},
+								{ label: "Square Meter", value: "sqmtr" },
+								{ label: "Square Feet", value: "sqft" },
+							]}
+							placeholder={{}}
+							style={pickerSelectStyles}
+						/>
+					</View>
+				</View>
+				<View style={styles.inputTextView}></View>
+			</View>
 		</View>
 	);
 }
+const styles = StyleSheet.create({
+	inputView: {
+		padding: 5,
+		backgroundColor: "white",
+		borderRadius: 4,
+		elevation: 1,
+	},
+	inputUnitView: {
+		flexDirection: "row",
+	},
+	inputUnitLabel: {
+		flex: 1,
+		marginRight: 2,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	inputUnitselectorView: {
+		flex: 3,
+		backgroundColor: "white",
+		borderWidth: 1,
+		borderColor: "#ddd",
+		borderRadius: 4,
+		marginLeft: 2,
+	},
+	inputTextView: {},
+});
+const pickerSelectStyles = StyleSheet.create({
+	inputIOS: {},
+	inputAndroid: {},
+});
