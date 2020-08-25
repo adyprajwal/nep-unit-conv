61fcc8db91501ea660377884152cfbd04feb3a0e
Prajwal Adhikari <prajwal.ady@gmail.com>
2020-08-25 12:41:01 +0545

    Show different input fields based on input unit selection

--- components/nepaliLandConverter/landInputField.js
+++ components/nepaliLandConverter/landInputField.js
@@ -2,63 +2,149 @@ import React from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
 
 export default function LandInputField({ unit, state, handleTextChange }) {
-	const renderRopani = () => {
-		if (unit == "rapd") {
-			return (
-				<View style={styles.unitRow}>
-					<View style={styles.unitBox}>
+	const renderInputField = (unit) => {
+		switch (unit) {
+			case "rapd": {
+				return (
+					<View style={styles.unitRow}>
+						<View style={styles.unitBox}>
+							<TextInput
+								style={styles.inputField}
+								value={state.ropani}
+								onChangeText={(text) =>
+									handleTextChange(text, { name: "ropani" })
+								}
+								placeholder="0"
+								keyboardType={"numeric"}
+							/>
+							<Text style={styles.inputFieldLabel}>Ropani</Text>
+						</View>
+						<View style={styles.unitBox}>
+							<TextInput
+								style={styles.inputField}
+								value={state.aana}
+								onChangeText={(text) =>
+									handleTextChange(text, { name: "aana" })
+								}
+								placeholder="0"
+								keyboardType={"numeric"}
+							/>
+							<Text style={styles.inputFieldLabel}>Aana</Text>
+						</View>
+						<View style={styles.unitBox}>
+							<TextInput
+								style={styles.inputField}
+								value={state.paisa}
+								onChangeText={(text) =>
+									handleTextChange(text, { name: "paisa" })
+								}
+								placeholder="0"
+								keyboardType={"numeric"}
+							/>
+							<Text style={styles.inputFieldLabel}>Paisa</Text>
+						</View>
+						<View style={styles.unitBox}>
+							<TextInput
+								style={styles.inputField}
+								value={state.daam}
+								onChangeText={(text) =>
+									handleTextChange(text, { name: "daam" })
+								}
+								placeholder="0"
+								keyboardType={"numeric"}
+							/>
+							<Text style={styles.inputFieldLabel}>Daam</Text>
+						</View>
+					</View>
+				);
+			}
+			case "bkd": {
+				return (
+					<View style={styles.unitRow}>
+						<View style={styles.unitBox}>
+							<TextInput
+								style={styles.inputField}
+								value={state.bigha}
+								onChangeText={(text) =>
+									handleTextChange(text, { name: "bigha" })
+								}
+								placeholder="0"
+								keyboardType={"numeric"}
+							/>
+							<Text style={styles.inputFieldLabel}>Bigha</Text>
+						</View>
+						<View style={styles.unitBox}>
+							<TextInput
+								style={styles.inputField}
+								value={state.kattha}
+								onChangeText={(text) =>
+									handleTextChange(text, { name: "kattha" })
+								}
+								placeholder="0"
+								keyboardType={"numeric"}
+							/>
+							<Text style={styles.inputFieldLabel}>Kattha</Text>
+						</View>
+						<View style={styles.unitBox}>
+							<TextInput
+								style={styles.inputField}
+								value={state.dhur}
+								onChangeText={(text) =>
+									handleTextChange(text, { name: "dhur" })
+								}
+								placeholder="0"
+								keyboardType={"numeric"}
+							/>
+							<Text style={styles.inputFieldLabel}>Dhur</Text>
+						</View>
+					</View>
+				);
+			}
+			case "sqmtr": {
+				return (
+					<View style={{ flexDirection: "row" }}>
 						<TextInput
-							style={styles.inputField}
-							value={state.ropani}
+							style={[
+								styles.inputField,
+								{ flex: 2, marginRight: 20 },
+							]}
+							value={state.sqmtr}
 							onChangeText={(text) =>
-								handleTextChange(text, { name: "ropani" })
+								handleTextChange(text, { name: "sqmtr" })
 							}
 							placeholder="0"
 							keyboardType={"numeric"}
 						/>
-						<Text style={styles.inputFieldLabel}>Ropani</Text>
+						<Text style={[styles.inputFieldLabel, { flex: 1 }]}>
+							Square Meter
+						</Text>
 					</View>
-					<View style={styles.unitBox}>
+				);
+			}
+			case "sqft": {
+				return (
+					<View style={{ flexDirection: "row" }}>
 						<TextInput
-							style={styles.inputField}
-							value={state.aana}
+							style={[
+								styles.inputField,
+								{ flex: 2, marginRight: 20 },
+							]}
+							value={state.sqft}
 							onChangeText={(text) =>
-								handleTextChange(text, { name: "aana" })
+								handleTextChange(text, { name: "sqft" })
 							}
 							placeholder="0"
 							keyboardType={"numeric"}
 						/>
-						<Text style={styles.inputFieldLabel}>Aana</Text>
+						<Text style={[styles.inputFieldLabel, { flex: 1 }]}>
+							Square Feet
+						</Text>
 					</View>
-					<View style={styles.unitBox}>
-						<TextInput
-							style={styles.inputField}
-							value={state.paisa}
-							onChangeText={(text) =>
-								handleTextChange(text, { name: "paisa" })
-							}
-							placeholder="0"
-							keyboardType={"numeric"}
-						/>
-						<Text style={styles.inputFieldLabel}>Paisa</Text>
-					</View>
-					<View style={styles.unitBox}>
-						<TextInput
-							style={styles.inputField}
-							value={state.daam}
-							onChangeText={(text) =>
-								handleTextChange(text, { name: "daam" })
-							}
-							placeholder="0"
-							keyboardType={"numeric"}
-						/>
-						<Text style={styles.inputFieldLabel}>Daam</Text>
-					</View>
-				</View>
-			);
+				);
+			}
 		}
 	};
-	return <View>{renderRopani()}</View>;
+	return <View>{renderInputField(unit)}</View>;
 }
 const styles = StyleSheet.create({
 	unitRow: {
